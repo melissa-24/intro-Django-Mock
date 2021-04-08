1327f7891e9ab91c16f8867dd6d123221e195d9f
dojo24 <mlongenberger@codingdojo.com>
2021-04-08 11:57:59 -0400

    pushing changes, still can't pull up about

--- mockProject/garden/urls.py
+++ mockProject/garden/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.index, name='index'),
-    path('/garden/', views.about, name='about'),
+    path('', views.about, name='about'),
 ]
--- mockProject/mockProject/urls.py
+++ mockProject/mockProject/urls.py
@@ -18,6 +18,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path('garden/', include('garden.urls')),
-    path('garden/about/', include('garden.urls')),
+    path('about/', include('garden.urls')),
     path('admin/', admin.site.urls),
 ]
